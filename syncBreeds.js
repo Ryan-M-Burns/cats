108c8fdf23f47f799413b1126dd3121c85c7d9a0
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 23:40:12 +0000

    cat breeds!

---
+++ syncBreeds.js
@@ -0,0 +1,23 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) {
+      functionToRunWhenThingsAreDone(data);
+    }
+
+    return console.log('error', error);
+  });
+
+};
+
+const printOutCatBreed = breed => {
+  return console.log('Return Value: ', breed);
+}
+
+breedDetailsFromFile('Bombay', printOutCatBreed);
